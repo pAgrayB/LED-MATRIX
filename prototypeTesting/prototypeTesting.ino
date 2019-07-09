f13e6ec8b754302d931128a1452feb41866d5efd
pAgrayB <ptgraybeal22.vt.edu>
2019-07-08 22:36:21 -0400

    Added prototyping arduino sketch.

---
+++ prototypeTesting/prototypeTesting.ino
@@ -0,0 +1,23 @@
+#include "LED-MATRIX.h"
+
+uint16_t pattern [4][4];
+
+void setup() {
+  matrix.init();
+  matrix.clear();
+
+  for (int row = 0; row < 4; row++)
+  {
+    for (int col = 0; col < 4; col++)
+    {
+      pattern[row][col] = 2000;
+    }
+  }
+
+}
+
+void loop() {
+  matrix.set(pattern);
+  matrix.update(1000, 3);
+
+}
